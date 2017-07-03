988efffb9a1827b6a91433ebcd753eb64c18a4a4
Skit5 <chapeau.plume@gmail.com>
2017-07-03 22:21:22 +0200

    Optimizer fonctionnel

--- ogr-server/optimizer.cpp
+++ ogr-server/optimizer.cpp
@@ -3,29 +3,43 @@
 namespace ogr{
 
     void optimizer(vector<param2optimize> params, const function<Mat()> &update){
-        bool flagOut = false;
 
-        namedWindow("Fenêtre d'optimisation",1);
+        /****************************
+        //  INITIALIZATION
+        ****************************/
+        string winName = "Fenêtre d'optimisation";
+
+        TrackbarCallback trackbarCb = [](int pos, void* userData){
+            (*(function<void(int)>*)userData)(pos);
+        };
+        function<void(int)> refresh = [&](int pos){
+                    imshow(winName,update());
+        };
+
+        /****************************
+        //  LANCEMENT DE LA FENËTRE
+        ****************************/
+        namedWindow(winName,1);
+        /// Ajout d'une trackbar pour chaque paramètre
         for(int p=0; p<params.size(); ++p){
-            createTrackbar(params[p].name, "Fenêtre d'optimisation",params[p].paramAddress,params[p].paramMax);
+            createTrackbar(params[p].name, winName,params[p].paramAddress,params[p].paramMax, trackbarCb, (void*)&refresh);
         }
-        do{
+        /// Affichage initial
+        refresh(0);
 
-            imshow("Fenêtre d'optimisation",update());
-            /****************************
-            //  TEST DE VALEUR DE SORTIE
-            ****************************/
-            /// Pause 14/sec pour tester le clavier
-            int keyPressed = cvWaitKey(72);
-            /// Si une touche est pressée
-            if(keyPressed >= 0){
-                /// extrait les 2B du code ascii du character
-                keyPressed = keyPressed&0xFFFF;
-                /// et teste ESC, ENTER, q  ou la fermeture de la fenêtre pour la sortie
-                if(keyPressed == 27 || keyPressed == 10 || keyPressed == 113 || (getWindowProperty("Fenêtre d'optimisation",0)>=0))
-                    flagOut = true;
-            }
-        }while(!flagOut);
+        /****************************
+        //  TEST DE VALEUR DE SORTIE
+        ****************************/
+        /// Pause 14/sec pour tester le clavier
+        int keyPressed = cvWaitKey();//72
+        /// Si une touche est pressée
+        if(keyPressed >= 0){
+            /// extrait les 2B du code ascii du character
+            keyPressed = keyPressed&0xFFFF;
+            /// et teste ESC, ENTER, q  ou la fermeture de la fenêtre pour la sortie
+            if(keyPressed == 27 || keyPressed == 10 || keyPressed == 113 || (getWindowProperty("Fenêtre d'optimisation",0)>=0))
+                destroyWindow(winName);
+        }
 
     }
 }
