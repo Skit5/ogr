948a1c1529a0033bd604447de149c1af358049a7
Skit5 <chapeau.plume@gmail.com>
2017-07-13 14:16:17 +0200

    On se prépare à séparer la reconnaissance des lignes et de la zone

--- ogr-server/pretreatment.cpp
+++ ogr-server/pretreatment.cpp
@@ -114,44 +114,44 @@ namespace ogr{
             cvtColor(edgedPicture,detectedLines,CV_GRAY2BGR);
             Vec4d borderPos={centerX,centerY,centerX,centerY},
                 borderLength={0,0,0,0}, borderLoc={0,0,0,0};
-            vector<double> histoLengthX(edgedPicture.cols), histoLocX(edgedPicture.cols),
-                histoLengthY(edgedPicture.rows), histoLocY(edgedPicture.rows);
+            vector<double> histoCoLocX(edgedPicture.cols), histoLocX(edgedPicture.cols),
+                histoCoLocY(edgedPicture.rows), histoLocY(edgedPicture.rows);
 
-            HoughLinesP(edgedPicture, lines, 1, CV_PI/180, *(params[0].paramAddress),
+            HoughLinesP(edgedPicture, lines, *(params[4].paramAddress)+1, CV_PI/180, *(params[0].paramAddress),
                 (double)*(params[1].paramAddress), (double)*(params[2].paramAddress));
             lineClassifier(lines,*(params[3].paramAddress),labels,
-                histoLengthX,histoLocX,histoLengthY,histoLocY);
+                histoCoLocX,histoLocX,histoCoLocY,histoLocY);
 
             Vec3d top,bot,left,right;
-            histo2Borders(*(params[4].paramAddress),histoLengthX,histoLocX,left,right);
-            histo2Borders(*(params[4].paramAddress),histoLengthY,histoLocY,bot,top);
+            histo2Borders(*(params[4].paramAddress),histoCoLocX,histoLocX,left,right);
+            histo2Borders(*(params[4].paramAddress),histoCoLocY,histoLocY,bot,top);
             if(DEBUG)
                 cout<<"== Detected boundaries =="<<endl;
             if(top[1]>0){
                 borderPos[3] = top[0];
-                borderLength[3] = top[1];
                 borderLoc[3] = top[2];
+                borderLength[3] = top[1] - top[2];
                 if(DEBUG)
-                    cout<<"T :"<<borderPos[3]<<" ";
+                    cout<<"T :"<<borderPos[3]<<" "<<borderLength[3]<<" "<<top[1] - top[2]<<" "<<top[1]<<" "<<top[2]<<" ";
             }
             if(bot[1]>0){
                 borderPos[1] = bot[0];
-                borderLength[1] = bot[1];
                 borderLoc[1] = bot[2];
+                borderLength[1] = bot[1] - bot[2];
                 if(DEBUG)
-                    cout<<"B :"<<borderPos[1]<<" ";
+                    cout<<"B :"<<borderPos[1]<<" "<<borderLength[1]<<" ";
             }
             if(left[1]>0){
                 borderPos[0] = left[0];
-                borderLength[0] = left[1];
                 borderLoc[0] = left[2];
+                borderLength[0] = left[1] - left[2];
                 if(DEBUG)
                     cout<<"L :"<<borderPos[0]<<" ";
             }
             if(right[1]>0){
                 borderPos[2] = right[0];
-                borderLength[2] = right[1];
                 borderLoc[2] = right[2];
+                borderLength[2] = right[1] - right[2];
                 if(DEBUG)
                     cout<<"R :"<<borderPos[2]<<" ";
             }
@@ -174,10 +174,11 @@ namespace ogr{
                     }else if(label==1){ /// horizontale
                         color = Scalar(0,255,0);
                     }else{ /// oblique
-                        color = Scalar(150,150,150);
+                        color = Scalar(170,170,0);
                     }
                     line( detectedLines, Point(l[0], l[1]), Point(l[2], l[3]), color, 1, CV_AA);
                 }
+                resize(detectedLines, detectedLines, detectedLines.size());
             }
             return detectedLines;
         });
@@ -186,22 +187,22 @@ namespace ogr{
     }
 
     void lineClassifier(vector<Vec4i> lines, int errAlign, vector<int> &labels,
-        vector<double> &histoLengthX, vector<double> &histoLocX,
-        vector<double> &histoLengthY, vector<double> &histoLocY){
+        vector<double> &histoCoLocX, vector<double> &histoLocX,
+        vector<double> &histoCoLocY, vector<double> &histoLocY){
 
         for( size_t i = 0; i < lines.size(); i++ ){
             Vec4i l = lines[i];
             int label = 0;
             double lineLength = sqrt( pow(l[2]-l[0], 2) + pow(l[1]-l[3], 2));
-            cout<<" l0="<<l[0]<<" l1="<<l[1]<<" l2="<<l[2]<<" l3="<<l[3]<<endl;
+            //cout<<" l0="<<l[0]<<" l1="<<l[1]<<" l2="<<l[2]<<" l3="<<l[3]<<endl;
             if(abs(l[0]-l[2])<= errAlign){
                 label = 2; /// verticale
-                histoLengthX[l[0]] = max(histoLengthX[l[0]],(double)(l[1]-l[3]));
-                histoLocX[l[0]] = min(histoLocX[l[0]], (double)l[1]);
+                histoCoLocX[l[0]] = max(histoCoLocX[l[0]],(double)l[1]);
+                histoLocX[l[0]] = min(histoLocX[l[0]], (double)l[3]);
             }
             else if(abs(l[1]-l[3])<= errAlign){
                 label = 1; /// horizontale
-                histoLengthY[l[1]] = max(histoLengthY[l[1]],(double)(l[2]-l[0]));
+                histoCoLocY[l[1]] = max(histoCoLocY[l[1]],(double)l[2]);
                 histoLocY[l[1]] = min(histoLocY[l[1]], (double)l[0]);
             }
             labels.push_back(label);
@@ -209,49 +210,48 @@ namespace ogr{
         return;
     }
 
-    void histo2Borders(int histoErr,
-        vector<double> histoLength, vector<double> histoLoc,
-        Vec3d &borderInf, Vec3d &borderSup){
+    void histo2Borders(int histoErr, vector<double> histoCoLoc,
+        vector<double> histoLoc, Vec3d &borderInf, Vec3d &borderSup){
 
-        if(histoLength.size() == histoLoc.size()){  /// On teste si les tailles sont cohérentes
-            vector<double> bufferLengthInf(histoErr+1), bufferLocInf(histoErr+1),
-                bufferLengthSup(histoErr+1), bufferLocSup(histoErr+1);
-            int binSizeInf = 0, binSizeSup = 0, middle = histoLength.size()/2;
+        if(histoCoLoc.size() == histoLoc.size()){  /// On teste si les tailles sont cohérentes
+            vector<double> bufferCoLocInf(histoErr+1), bufferLocInf(histoErr+1),
+                bufferCoLocSup(histoErr+1), bufferLocSup(histoErr+1);
+            int middle = histoLoc.size()/2;
             bool hasInf = false, hasSup = false;
             for(int i=0; ((i<middle) && (!hasSup||!hasInf)); ++i){
                 if(!hasInf){ /// Si on a pas encore trouvé le bord inférieur
-                    if(histoLength[i]>0){ /// On teste la présence d'un bord
+                    if(histoCoLoc[i]>0){ /// On teste la présence d'un bord
                         int iEnd = i+histoErr;
-                        double posSum = 0, locMin = 0, longMax = 0, nbrPos = 0, posMean = 0;
+                        double posSum = 0, locMin = middle, coLocMax = middle, nbrPos = 0, posMean = 0;
                         for(int j=i; j<iEnd; ++j){
                         /// Pos est moyenné
                         /// Loc est le min
                         /// Long est le max
-                            longMax = max(longMax, histoLength[j]);
+                            coLocMax = max(coLocMax, histoCoLoc[j]);
                             locMin = min(locMin, histoLoc[j]);
-                            if(histoLength[j]>0){
+                            if(histoCoLoc[j]>0){
                                 ++nbrPos;
                                 posSum += j;
                             }
                         }
-                        borderInf = {round(posSum/nbrPos),longMax,locMin};
+                        borderInf = {round(posSum/nbrPos),coLocMax,locMin};
                         hasInf = true;
                     }
                 }
                 if(!hasSup){ /// Si on a pas encore trouvé le bord supérieur
-                    int iInv = (histoLength.size()-1)-i;
-                    if(histoLength[iInv]>0){
+                    int iInv = (histoCoLoc.size()-1)-i;
+                    if(histoCoLoc[iInv]>0){
                         int iStart = iInv-histoErr;
-                        double posSum = 0, locMin = 0, longMax = 0, nbrPos = 0, posMean = 0;
+                        double posSum = 0, locMin = middle, coLocMax = middle, nbrPos = 0, posMean = 0;
                         for(int j=iStart; j<iInv; ++j){
-                            longMax = max(longMax, histoLength[j]);
+                            coLocMax = max(coLocMax, histoCoLoc[j]);
                             locMin = min(locMin, histoLoc[j]);
-                            if(histoLength[j]>0){
+                            if(histoCoLoc[j]>0){
                                 ++nbrPos;
                                 posSum += j;
                             }
                         }
-                        borderSup = {round(posSum/nbrPos),longMax,locMin};
+                        borderSup = {round(posSum/nbrPos),coLocMax,locMin};
                         hasSup = true;
                     }
                 }
